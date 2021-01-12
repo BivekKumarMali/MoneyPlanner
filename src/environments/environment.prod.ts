cf2926c50809ff265b70c3f65688093a70fbf7f3
BivekKumarMali <bivekkumar@promactinfo.com>
2021-01-12 15:43:08 +0530

    add firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAEP6f6TRrqwKdnS-Y1LFlUVk3pDnJV2Og',
+    authDomain: 'monthly-planner-63785.firebaseapp.com',
+    databaseURL: 'https://monthly-planner-63785.firebaseio.com',
+    projectId: 'monthly-planner-63785',
+    storageBucket: 'monthly-planner-63785.appspot.com',
+    messagingSenderId: '483380858149',
+    appId: '1:483380858149:web:bcb35364db5a5a649b5ecc',
+    measurementId: 'G-N9YC9X9LYV'
+  }
 };
